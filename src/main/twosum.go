9e7a5e1c74ab60f10b9102a0c209fada67a7d81c
wanghongxing <hongxingwang@sohu-inc.com>
2017-01-31 23:21:14 +0800

    init leetcode project and submit two sum

---
+++ src/main/twosum.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	start := time.Now().UnixNano()
+	nums := []int{0, 4, 3, 0}
+	index := twoSum2(nums, 0)
+	fmt.Println(index)
+	end := time.Now().UnixNano()
+	fmt.Println((end - start) / 1000)
+}
+
+func twoSum(nums []int, target int) []int {
+
+	for w, wVal := range nums {
+		for i, iVal := range nums {
+			if iVal + wVal == target && i != w {
+				return []int{w, i}
+			}
+		}
+	}
+
+	return nil
+}
+
+func twoSum2(nums []int, target int, ) []int {
+
+	visited := make(map[int]int)
+
+	for index, val := range nums {
+		preIndex, ok := visited[target - val]
+		if ok {
+			return []int{preIndex, index}
+		} else {
+			visited[val] = index
+		}
+	}
+
+	return nil
+
+}
